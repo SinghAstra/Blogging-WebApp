86576c12ad63d99bbecfcff425d620bb6b668ef4
Astra <abhaypratapsinghwd@gmail.com>
2024-03-20 16:40:54 +0530

    Blog UI

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -3,7 +3,11 @@ import React from "react";
 
 const BlogPage = () => {
   return (
-    <div className="flex justify-evenly">
+    <div className="flex justify-evenly min-h-screen bg-black flex-wrap">
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
+      <BlogCard />
       <BlogCard />
       <BlogCard />
       <BlogCard />
--- src/components/BlogCard/BlogCard.js
+++ src/components/BlogCard/BlogCard.js
@@ -5,9 +5,9 @@ import "./BlogCard.css";
 
 const BlogCard = () => {
   return (
-    <div className="bg-green-400 w-60">
-      <div className="relative w-full border border-red-400">
-        <div className="relative image-container bg-pink-400 h-60">
+    <div className="bg-blue-400 w-72 font-mono text-white h-fit p-2 m-2 shadow-sm shadow-blue-300">
+      <div className="w-full flex">
+        <div className="relative image-container h-64">
           <Image
             src={
               "https://images.unsplash.com/photo-1683009427598-9c21a169f98f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"
@@ -16,12 +16,16 @@ const BlogCard = () => {
             alt="blog"
           />
         </div>
-        <span className="bg-yellow-400 date-container">21 Dec,2021</span>
+        <div className="date-container flex justify-center items-center">
+          <span className="text-sm">21 Dec,2021</span>
+        </div>
       </div>
-      <div>
-        <h1>Title</h1>
-        <p>Description</p>
-        {/* <Link>READ MORE</Link> */}
+      <div className="flex flex-col gap-1 p-1">
+        <h1 className="text-xl">Title</h1>
+        <p className="text-sm">Description</p>
+        <Link href={`/blog/123`} className="underline">
+          READ MORE
+        </Link>
       </div>
     </div>
   );
